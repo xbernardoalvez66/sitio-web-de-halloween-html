cbafee24944ea554c2a1b5d9af60e1a7c618d521
xbernardoalvez66 <55824722+xbernardoalvez66@users.noreply.github.com>
2022-12-01 02:20:28 -0300

    Add files via upload

---
+++ sitio-web-de-halloween-html/js/script.js
@@ -0,0 +1,8 @@
+AOS.init();
+
+let menu_bar = document.getElementById('menu_bar');
+let menu = document.getElementById('menu');
+
+menu_bar.addEventListener('click', function(){
+    menu.classList.toggle('show-menu')
+})
